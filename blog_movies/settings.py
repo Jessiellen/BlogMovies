f8160a49a40ae931fb8d4ee83faf43b645cd2ee3
Jessie <146772693+Jessiellen@users.noreply.github.com>
2024-04-24 22:30:45 +0000

    update

--- blog_movies/settings.py
+++ blog_movies/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL = 'post-list' 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
